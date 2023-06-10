b8f80603329147cd0ce5a81c7ef3a050b37e2962
Martin <martinmicheletti@hotmail.com>
2023-06-10 20:53:37 -0300

    querys + codigo

--- codigo.js
+++ codigo.js
@@ -1,56 +1,172 @@
 // Lista de estados posibles para una transferencia.
 const statuses = [
-    {
-        id: 1,
-        name: 'CREADA'
-    },
-    {
-        id: 2,
-        name: 'PAGADA'
-    },
-    {
-        id: 3,
-        name: 'ABORTADA'
-    },
-    {
-        id: 4,
-        name: 'FINALIZADA'
-    }
+  {
+    id: 1,
+    name: "CREADA",
+  },
+  {
+    id: 2,
+    name: "PAGADA",
+  },
+  {
+    id: 3,
+    name: "ABORTADA",
+  },
+  {
+    id: 4,
+    name: "FINALIZADA",
+  },
 ];
 
 // Lista de transferencias inicial
 const transfers = [
-    {
-        id: 1,
-        licensePlate: 'LFTS34',
-        email: 'usuario1@autored.cl',
-        status: 'CREADA',
-    },
-    {
-        id: 2,
-        licensePlate: 'LFTS35',
-        email: 'usuario1@autored.cl',
-        status: 'ABORTADA',
-    },
-    {
-        id: 3,
-        licensePlate: 'BDLS99',
-        email: 'usuario3@autored.cl',
-        status: 'CREADA',
-    },
-    {
-        id: 4,
-        licensePlate: 'LFTS34',
-        email: 'usuario4@autored.cl',
-        status: 'CREADA',
-    },
-    {
-        id: 5,
-        licensePlate: 'BDLS99',
-        email: 'usuario5@autored.cl',
-        status: 'FINALIZADA',
-    }
+  {
+    id: 1,
+    licensePlate: "LFTS34",
+    email: "usuario1@autored.cl",
+    status: "CREADA",
+  },
+  {
+    id: 2,
+    licensePlate: "LFTS35",
+    email: "usuario1@autored.cl",
+    status: "ABORTADA",
+  },
+  {
+    id: 3,
+    licensePlate: "BDLS99",
+    email: "usuario3@autored.cl",
+    status: "CREADA",
+  },
+  {
+    id: 4,
+    licensePlate: "LFTS34",
+    email: "usuario4@autored.cl",
+    status: "CREADA",
+  },
+  {
+    id: 5,
+    licensePlate: "BDLS99",
+    email: "usuario5@autored.cl",
+    status: "FINALIZADA",
+  },
 ];
 
+// Escribe tu codigo acá
 
-// Escribe tu codigo acá 
+// - Crear una función para listar
+// todas las transferencias de un usuario mediante su correo.
+const getTransferenciasUsuario = (correo) =>
+  transfers.filter((x) => x.email === correo);
+// - Crear una función para crear una nueva transferencia
+// validando que todos los datos sean ingresados correctamente.
+
+/*
+- Una patente válida tiene las siguientes características:
+    * AAAA00 => **4 letras y 2 números**.
+    * AA0000 => **2 letras y 4 números**.
+*/
+const licenseRegex = /^([A-Z]{4}\d{2})|([A-Z]{2}\d{4})$/;
+const validateLicenceFormat = (licencePlate) => licenseRegex.test(licencePlate);
+
+const getNewId = () => {
+  const ids = transfers?.map((x) => x.id);
+  const sorted = ids?.sort((a, b) => a - b);
+  return sorted[0] + 1;
+};
+
+const isAnyTransferPayed = (licensePlate) => {
+  const licenceTransfers = transfers?.filter(
+    (x) => x.licensePlate === licensePlate
+  );
+  const payedTransfer = licenceTransfers?.find((x) => x.status === "PAGADA");
+  return payedTransfer ? true : false;
+};
+
+const findTransfersBy = (email, licensePlate) =>
+  transfers?.find((x) => x.licensePlate === licensePlate && x.email === email);
+const filterTransfersBy = (email, licensePlate) =>
+  transfers?.filter(
+    (x) => x.licensePlate === licensePlate && x.email === email
+  );
+
+const isAnyTransfer = (email, licensePlate) => {
+  const existingTransfers = filterTransfersBy(email, licensePlate);
+  const endedTransfers = existingTransfers?.find(
+    (x) => x.status === "FINALIZADA" || x.status === "ABORTADA"
+  );
+
+  return !existingTransfers ? false : endedTransfers ? true : false;
+};
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const validateEmail = (email) => emailRegex.test(email);
+
+const abortOtherLicenseTransfers = (licensePlate) => {
+  const licenseTransfers = transfers?.filter(
+    (x) => x.licensePlate === licensePlate
+  );
+  const payedLicense = licenseTransfers?.find((x) => x.status == "PAGADA");
+  if (!payedLicense) throw new Error("No se pueden abortar las transferencias");
+
+  // ver si muta transfers
+  licenseTransfers.forEach((x) => {
+    if (x.status !== "PAGADA") {
+      x.status = "ABORTADA";
+    }
+  });
+};
+
+// Crear una función para pagar una transferencia mediante correo y patente.
+const payTransfer = (email, licensePlate) => {
+  const existingTransfer = findTransfersBy(email, licensePlate);
+  if (!existingTransfer)
+    throw new Error("No existen transferencias para los datos indicados");
+
+  // ver si muta
+  existingTransfer.status == statuses?.find((x) => x.id === 2).name;
+
+  /*
+    - Si hay múltiples transferencias con misma patente 
+    y distinto correo, y una de estas transferencias 
+    avanza al estado **'PAGADA'**, entonces todas las otras 
+    transferencias cambian al estado **'ABORTADA'**.
+*/
+  abortOtherLicenseTransfers(licensePlate);
+
+  return existingTransfer;
+};
+
+const addTransfer = (payload) => {
+  // licencePlate
+  // validar patente valida
+  if (!validateLicenceFormat(payload?.licensePlate))
+    throw new Error("La patente debe tener un formato correcto");
+
+  // si de todas las transferencias, para una patente
+  // alguna esta  'PAGADA', no se permite crear mas
+  if (isAnyTransferPayed(payload?.licensePlate))
+    throw new Error("No se aceptan mas transferencias para la patente");
+
+  // email
+  // validar email con formato
+  if (!validateEmail(payload?.email))
+    throw new Error("El email debe tener un formato correcto");
+
+  // validar existencia de misma patente y correo
+  // - si la q existe, esta en estado 'FINALIZADA' o 'ABORTADA'
+  // -- se permite crear otra
+  if (isAnyTransfer(payload?.email, payload?.licensePlate))
+    throw new Error(
+      "Ya hay registradas transferencias para el email y patente indicados"
+    );
+
+  const transfer = {
+    id: getNewId(),
+    licensePlate: payload?.licensePlate,
+    email: payload?.email,
+    status: statuses?.find((x) => x.id === 1).name,
+  };
+  transfers.push(transfer);
+  return transfer;
+};
--- querys.sql
+++ querys.sql
@@ -1,1 +1,72 @@
 -- Escribe tus querys acá
+
+-- CLIENTE
+---correo
+
+-- VEHICULO
+---patente
+
+-- HISTORIAL_MOTIVO
+
+-- MOTIVO
+
+-- PAGO
+
+-- PASARELA_PAGO
+
+-- INFORME
+---fechaEmision
+---precioVenta
+
+-- HISTORIAL_ESTADO
+
+-- ESTADO
+
+-- 
+
+-- QUERYS
+-- - Se requiere el correo de todos los clientes 
+-- que hayan comprado más de un informe 
+-- a la vez mediante la pasarela de pago 'transbank'.
+
+-- CONSIDERANDO TABLA INTERMEDIA
+SELECT C.CORREO
+FROM PAGOS P
+JOIN INFORMES I ON P.ID_INFORME = I.ID_INFORME
+JOIN INFORMES_X_CLIENTE IXC ON IXC.ID_INFORME = I.ID_INFORME
+JOIN CLIENTES C ON IXC.ID_CLIENTE = C.ID_CLIENTE
+WHERE PP.NOMBRE LIKE 'transbank' 
+GROUP BY C.CORREO
+HAVING COUNT(*) >= 2;
+--
+SELECT C.CORREO 
+FROM PAGOS P
+JOIN INFORMES I ON P.ID_INFORME = I.ID
+JOIN CLIENTES C ON P.I.ID_CLIENTE = C.ID
+JOIN PASARELA_PAGO PP ON P.ID_PASARELA_PAGO = PP.ID
+WHERE PP.NOMBRE LIKE 'transbank' 
+
+/*
+Devuelve todos los informes comprados que han sido entregados 
+al cliente juan_daniel@gmail.com **sin utilizar JOIN**
+*/
+
+-- ids informes comprados
+CREATE TEMPORARY TABLE temp_ids (ID_INFORME INT);
+
+-- OBTENGO ID DE CLIENTE CON DETERMINADO EMAIL
+DECLARE @specificID INT;
+
+SELECT @specificID = C.ID
+FROM CLIENTES C
+WHERE C.CORREO LIKE 'juan_daniel@gmail.com';
+
+INSERT INTO temp_ids (ID_INFORME)
+SELECT P.ID_INFORME, P.ID_CLIENTE
+FROM PAGOS P
+WHERE P.ID_CLIENTE = @specificID;
+
+-- consulto los informes comprados del cliente juan_daniel@gmail.com
+SELECT *
+FROM INFORMES
+WHERE ID IN (SELECT ID_INFORME AS 'ID' FROM temp_ids);
