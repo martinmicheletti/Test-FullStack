83176421fb965b5752157cba84dca2dd48b2b791
Martin <martinmicheletti@hotmail.com>
2023-06-15 18:49:36 -0300

    querys

--- querys.sql
+++ querys.sql
@@ -3,45 +3,34 @@
 -- Se requiere el correo de todos los clientes 
 -- que hayan comprado más de un informe 
 -- a la vez mediante la pasarela de pago 'transbank'.
-
--- CONSIDERANDO TABLA INTERMEDIA
-SELECT C.CORREO
+SELECT C.CORREO 
 FROM PAGOS P
 JOIN INFORMES I ON P.ID_INFORME = I.ID_INFORME
-JOIN INFORMES_X_CLIENTE IXC ON IXC.ID_INFORME = I.ID_INFORME
-JOIN CLIENTES C ON IXC.ID_CLIENTE = C.ID_CLIENTE
--- mejor por id
-WHERE PP.NOMBRE LIKE 'transbank' 
+JOIN CLIENTES C ON I.ID_CLIENTE = C.ID_CLIENTE
+JOIN PASARELAS_PAGO PP ON P.ID_PASARELA_PAGO = PP.ID_PASARELA_PAGO
+WHERE PP.NOMBRE LIKE 'transbank'
 GROUP BY C.CORREO
 HAVING COUNT(*) >= 2;
---
--- SELECT C.CORREO 
--- FROM PAGOS P
--- JOIN INFORMES I ON P.ID_INFORME = I.ID
--- JOIN CLIENTES C ON P.I.ID_CLIENTE = C.ID
--- JOIN PASARELA_PAGO PP ON P.ID_PASARELA_PAGO = PP.ID
--- WHERE PP.NOMBRE LIKE 'transbank' 
-
 
 --Devuelve todos los informes comprados que han sido entregados 
 --al cliente juan_daniel@gmail.com **sin utilizar JOIN**
 
--- ids informes comprados
-CREATE TEMPORARY TABLE temp_ids (ID_INFORME INT);
-
--- OBTENGO ID DE CLIENTE CON DETERMINADO EMAIL
+CREATE TEMPORARY TABLE temp_informes_ids (ID_INFORME INT);
 DECLARE @specificID INT;
 
 SELECT @specificID = C.ID
 FROM CLIENTES C
 WHERE C.CORREO LIKE 'juan_daniel@gmail.com';
 
-INSERT INTO temp_ids (ID_INFORME)
-SELECT P.ID_INFORME, P.ID_CLIENTE
+INSERT INTO temp_informes_ids (ID_INFORME)
+SELECT P.ID_INFORME
 FROM PAGOS P
 WHERE P.ID_CLIENTE = @specificID;
 
 -- consulto los informes comprados del cliente juan_daniel@gmail.com
-SELECT *
-FROM INFORMES
-WHERE ID_INFORME IN (SELECT ID_INFORME AS 'ID' FROM temp_ids);
+SELECT I.ID_INFORME, I.FECHA_EMISION, I.ID_CLIENTE, I.ID_VEHICULO, I.PRECIO
+FROM INFORMES I
+WHERE I.ID_INFORME IN (SELECT ID_INFORME FROM temp_informes_ids);
+
+-- se podria acceder al historial de motivos del vehiculo incluidos hasta ese momento en el informe
+-- tabla: HISTORIAL_MOTIVOS_VEHICULO_X_INFORME
